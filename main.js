0a250a5ae681d88fc6a788d444789278a8ffabaa
Gayun Lee <gayunlee11@gmail.com>
2021-10-28 00:50:55 +0900

    drag and drop: invisible when drop

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const $button = document.querySelector(".button");
 const $main = document.querySelector(".main");
+const $cookie = document.querySelector(".cookie");
 
 $button.addEventListener("click", scrollDown);
 $main.addEventListener("click", openSection);
+$cookie.addEventListener("dragstart", dragStart)
+$cookie.addEventListener("drag", dragCookie)
+$cookie.addEventListener("dragend", dragEnd)
 
 function scrollDown() {
   $main.scrollIntoView({behavior: "smooth"});
@@ -18,8 +22,22 @@ function openSection(ev) {
     target.classList.toggle('open');
   }
 
-
-
 }
 
+function dragCookie(ev) {
+  const x = ev.offsetX;
+  const y = ev.offsetY;
+  console.log(x,y)
+  $cookie.style.left=x;
+  $cookie.style.top=y;
+}
+
+function dragStart() {
+  this.className += 'hold';
+  setTimeout(()=> this.className = 'invisible',0)
+}
+
+function dragEnd() {
+  console.log('end')
+}
 
