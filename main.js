939f8fd3cb08622c9b5743af5375f30799ebdfd5
Gayun Lee <gayunlee11@gmail.com>
2021-10-28 01:58:33 +0900

    drag and drop

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const $main = document.querySelector(".main");
 const $cookie = document.querySelector(".cookie");
 const $cookieMonster = document.querySelector(".cookiemonster_eat");
 const $yum = document.querySelector(".yum");
+const $nope = document.querySelector(".nope");
+const $notCookies = document.querySelectorAll(".not-cookie");
 
 $button.addEventListener("click", scrollDown);
 $main.addEventListener("click", openSection);
@@ -12,7 +14,7 @@ $cookie.addEventListener("dragend", dragEnd);
 $cookieMonster.addEventListener("dragenter", dragEnter);
 $cookieMonster.addEventListener("dragover", dragOver);
 $cookieMonster.addEventListener("drop", dragDrop)
-
+$notCookies.forEach((notCookie) => {notCookie.addEventListener("dragstart", dragStart)});
 function scrollDown() {
   $main.scrollIntoView({behavior: "smooth"});
 }
@@ -29,31 +31,53 @@ function openSection(ev) {
 
 }
 
+let isCookie = false;
 
-function dragStart() {
+function dragStart(ev) {
+  // isCookie = ev.target.dataset.isCookie;
+  ev.target.dataset.cookie ? isCookie=true: isCookie=false;
+  console.log(ev.target.dataset.cookie)
   this.className += 'hold';
   setTimeout(()=> this.className = 'invisible',0)
 }
 
-function dragEnd() {
-  console.log("dragEnd")
+function dragEnd(ev) {
+  // isCookie = ev.target.dataset.cookie;
+  console.log(isCookie)
 }
 
 function dragEnter(ev) {
   ev.preventDefault();
+  ev.target.dataset.cookie ? isCookie=true: isCookie=false;
+  console.log('enter'+isCookie)
+
   // this.className += 'hovered';
   console.log("enter")
 }
 
 function dragOver(ev) {
   ev.preventDefault();
+  ev.target.dataset.cookie ? isCookie=true: isCookie=false;
+  console.log('enter'+isCookie)
   console.log("over")
+  console.log('over'+isCookie)
 }
 
 function dragDrop() {
-  $yum.classList.remove('hidden');
-  setTimeout(()=>{
-    $yum.classList.add('hidden');
-  },1000);
-}
+  console.log('drop'+isCookie)
+  if (isCookie) {
+    $yum.classList.remove('hidden');
+    setTimeout(()=>{
+      $yum.classList.add('hidden');
+    },1000);
+  } else {
+    console.log("nope")
+    if (isCookie) {
+      $nope.classList.remove('hidden');
+      setTimeout(()=>{
+        $nope.classList.add('hidden');
+      },1000);
+  }
 
+}
+}
